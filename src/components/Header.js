85c8f37ecf5502a0e856eb8aa25a52ac88e7b590
Sebastián Saldaña <A01570274@itesm.mx>
2020-06-25 16:35:40 -0500

    Finished design and initial styling of Navbar and Header components. Navbar still missing links. Will be added later to make scrollable page

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+import '../index.css'
+function Header() {
+    return(
+        <header className="masthead">
+            <div className="container h-100">
+                <div className="row h-100 align-items-center">
+                <div className="col-12 text-center">
+                    <h1 className="display-2 text-light font-weight-dark">Artículos únicos en su clase</h1>
+                    <p className="text-light lead">Tablets y handhelds de uso rudo, soluciones de Voz sobre IP, consultoría y mucho más</p>
+                    <Button variant="outline-light">Ver catálogo</Button>
+                </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React , { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Logo from '../assets/img/logo_final.png'
+
+class NavBar extends Component {
+    render(){
+        return(
+            <Navbar sticky="top" bg="light" variant="light">
+                <Navbar.Brand className="align-middle">
+                    <img src={ Logo }
+                     width="104"
+                     height="45"
+                     className="d-inline-block align-top"
+                     alt=""/>{' '}
+                     <span className="ml-3 align-middle">Securitas México</span>
+                </Navbar.Brand>
+                <Nav className="ml-auto">
+                    <Nav.Link>Inicio</Nav.Link>
+                    <Nav.Link>Catálogo</Nav.Link>
+                    <Nav.Link>Sobre nosotros</Nav.Link>
+                    <Nav.Link>Contacto</Nav.Link>
+                </Nav>
+            </Navbar>
+        )
+    }
+}
+
+export default NavBar
