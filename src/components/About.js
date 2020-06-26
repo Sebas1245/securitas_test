ef5f097fb04b221bf594614e0f0801c63dfaaaf9
Sebastián Saldaña <A01570274@itesm.mx>
2020-06-26 16:56:31 -0500

    Created About component

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
+import AboutThumbnail from '../assets/img/aboutThumbnail.jpg'
+
+function About(){
+    return(
+
+        <Container className="fluid my-4 text-primary">
+            <Row className="justify-content-center"> 
+                <h1>Quiénes somos</h1>
+            </Row>
+            <Row className="justify-content-center">
+                <h5><em>Un poco sobre nosotros</em></h5>
+            </Row>
+            <Row>
+                <p className="text-justify text-dark lead">En <strong className="text-primary">Securitas México</strong> tenemos la visión de proveer productos y servicios <strong className="text-primary">innovadores</strong> de la más alta calidad. Somos <strong className="text-primary">consultores</strong> de productos y servicios que marcan tendencia,  de primer nivel y proveemos un <strong className="text-primary">gran servicio al cliente</strong>. Somos un negocio compuesto por personas innovadoras que siempre miran a futuro. Tenemos el impulso y los medios para actualizar y mejorar constantemente la experiencia de tu compra en línea.</p>
+            </Row>
+            <Row className="justify-content-center">
+                <Image width="80%" src={AboutThumbnail} rounded fluid />
+            </Row>
+        </Container>
+    )
+}
+
+export default About
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
             <div className="container h-100">
                 <div className="row h-100 align-items-center">
                 <div className="col-12 text-center">
-                    <h1 className="display-2 text-light font-weight-dark">Artículos únicos en su clase</h1>
+                    <h1 className="display-2 text-light font-weight-dark">Artículos y servicios únicos en su clase</h1>
                     <p className="text-light lead">Tablets y handhelds de uso rudo, soluciones de Voz sobre IP, consultoría y mucho más</p>
                     <Button variant="outline-light">Ver catálogo</Button>
                 </div>
