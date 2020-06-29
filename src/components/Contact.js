385a60713d1b81bb6b8ce48f0c8e60873681d630
Sebastián Saldaña <A01570274@itesm.mx>
2020-06-29 14:57:52 -0500

    Initial design of Contact component finished, still missing a way of sending data somewhere

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ContactForm from './ContactForm';
 // logo imports
 import { FaPhone , FaFacebook } from 'react-icons/fa';
 
@@ -15,12 +16,12 @@ function Contact(){
             <p className="text-dark lead">¿Necesitas nuestro apoyo? Estamos aquí para servirte.</p>
         </Row>
         <Row>
-            <Col xs={12} md={6} className="font-weight-light">
+            <Col xs={12} md={6} className="font-weight-light pt-5">
                 <Row className="justify-content-center">
                     <p>Calle Ecuador 518</p>
                 </Row>
                 <Row className="justify-content-center">
-                    <p>Monterrey,Nuevo León, México</p>
+                    <p>Monterrey, Nuevo León, México</p>
                 </Row>
                 <Row className="justify-content-center">
                     <p> <FaPhone /> +52 81 8682 7833</p>
@@ -30,6 +31,7 @@ function Contact(){
                 </Row>
             </Col>
             <Col xs={12} md={6}>
+                <ContactForm />
             </Col>
         </Row>
     </Container>
---
+++ src/components/ContactForm.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+class ContactForm extends Component {
+    state = {
+        name: '',
+        organization: '',
+        phone: '',
+        email: '',
+        subject: '',
+        message: '',
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        let form = document.getElementById('contact-form');
+        form.reset();
+    }
+    render() {
+        return (
+            <Form id="contact-form" className="m-2" onSubmit={this.handleSubmit}>
+                <Form.Row className="m-2">
+                    <Col>
+                        <Form.Control id="name" type="text" placeholder="Nombre" onChange={this.handleChange}></Form.Control>
+                    </Col>
+                    <Col>
+                        <Form.Control id="organization" type="text" placeholder="Empresa u organización" onChange={this.handleChange}></Form.Control>
+                    </Col>
+                </Form.Row>
+                <Form.Row className="m-2">
+                    <Col>
+                        <Form.Control id="phone" type="text" placeholder="Teléfono" onChange={this.handleChange}></Form.Control>
+                    </Col>
+                    <Col>
+                        <Form.Control id="email" type="email" placeholder="Correo" onChange={this.handleChange}></Form.Control>
+                    </Col>
+                </Form.Row>
+                <Form.Row className="m-2">
+                    <Col>
+                        <Form.Control id="subject" className="mx-auto my-2" type="text" placeholder="Asunto" onChange={this.handleChange}></Form.Control>
+                        <Form.Control id="message" as="textarea" rows="5" placeholder="Escríbenos tu mensaje" onChange={this.handleChange}></Form.Control>
+                    </Col>
+                </Form.Row>
+                <Form.Row className="justify-content-center">
+                    <Button type="submit" variant="warning" className="text-light">Enviar</Button>
+                </Form.Row>
+            </Form>
+
+        )
+    }
+}
+
+export default ContactForm
