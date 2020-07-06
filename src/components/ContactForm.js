e4af7f0071066673b1ad47f4ece4670e85435d87
Sebastián Saldaña <A01570274@itesm.mx>
2020-07-05 20:59:27 -0500

    Added validation to contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,6 +12,7 @@ class ContactForm extends Component {
         email: '',
         subject: '',
         message: '',
+        validated: false,
     }
     handleChange = (e) => {
         this.setState({
@@ -22,60 +23,71 @@ class ContactForm extends Component {
         document.getElementById('contact-form').reset();
     }
     handleSubmit = (e) => {
-        e.preventDefault();
-        const name = this.state.name;
-        const organization = this.state.organization;
-        const phone = this.state.phone;
-        const email = this.state.email;
-        const subject = this.state.subject;
-        const message = this.state.message;
-        axios({
-            method: 'POST',
-            url:"http://localhost:5000/send",
-            data: {
-                name,
-                organization,
-                phone,
-                email,
-                subject,
-                message
-            }
-        }).then((response)=> {
-            if(response.data.msg === 'success') {
-                // DESIGN USER FEEDBACK BEFORE CLEARING FORM
-                alert('Email sent!');
-                this.clearForm();
-            }
-            else if(response.data.msg === 'fail'){
-                alert("Message failed to send");
-            }
+        const form = e.currentTarget;
+        if(form.checkValidity() === false){
+            e.preventDefault();
+            e.stopPropagation();
+        }else{
+            const name = this.state.name;
+            const organization = this.state.organization;
+            const phone = this.state.phone;
+            const email = this.state.email;
+            const subject = this.state.subject;
+            const message = this.state.message;
+            axios({
+                method: 'POST',
+                url:"http://localhost:5000/send",
+                data: {
+                    name,
+                    organization,
+                    phone,
+                    email,
+                    subject,
+                    message
+                }
+            }).then((response)=> {
+                if(response.data.msg === 'success') {
+                    // DESIGN USER FEEDBACK BEFORE CLEARING FORM
+                    document.querySelector("#responseMessage").style.display = 'block';
+                    this.clearForm();
+                }
+                else if(response.data.msg === 'fail'){
+                    alert("Error al enviar mensaje");
+                }
+            })
+        }
+        this.setState({
+            validated: true
         })
-
     }
     render() {
         return (
-            <Form id="contact-form" className="m-2" onSubmit={this.handleSubmit}>
+            <Form id="contact-form" className="m-2" onSubmit={this.handleSubmit} noValidate validated={this.state.validated}>
                 <Form.Row className="m-2">
                     <Col>
-                        <Form.Control id="name" type="text" placeholder="Nombre" onChange={this.handleChange}></Form.Control>
+                        <Form.Control id="name" type="text" placeholder="Nombre" onChange={this.handleChange} required></Form.Control>
                     </Col>
                     <Col>
-                        <Form.Control id="organization" type="text" placeholder="Empresa u organización" onChange={this.handleChange}></Form.Control>
+                        <Form.Control id="organization" type="text" placeholder="Empresa u organización" onChange={this.handleChange} required></Form.Control>
                     </Col>
                 </Form.Row>
                 <Form.Row className="m-2">
                     <Col>
-                        <Form.Control id="phone" type="text" placeholder="Teléfono" onChange={this.handleChange}></Form.Control>
+                        <Form.Control id="phone" type="text" placeholder="Teléfono" onChange={this.handleChange} required></Form.Control>
                     </Col>
                     <Col>
-                        <Form.Control id="email" type="email" placeholder="Correo" onChange={this.handleChange}></Form.Control>
+                        <Form.Control id="email" type="email" placeholder="Correo" onChange={this.handleChange} required></Form.Control>
                     </Col>
                 </Form.Row>
                 <Form.Row className="m-2">
                     <Col>
-                        <Form.Control id="subject" className="mx-auto my-2" type="text" placeholder="Asunto" onChange={this.handleChange}></Form.Control>
-                        <Form.Control id="message" as="textarea" rows="5" placeholder="¿Cómo te podemos ayudar?" onChange={this.handleChange}></Form.Control>
+                        <Form.Control id="subject" className="mx-auto my-2" type="text" placeholder="Asunto" onChange={this.handleChange} required></Form.Control>
+                        <Form.Control id="message" as="textarea" rows="5" placeholder="¿Cómo te podemos ayudar?" onChange={this.handleChange} required></Form.Control>
+                        <Form.Control.Feedback type="invalid">Debes llenar todos los campos</Form.Control.Feedback>
+                        <div id="responseMessage" style={{display: 'none'}} className="text-success">¡Tu mensaje se envió exitosamente!</div>
                     </Col>
+                </Form.Row>
+                <Form.Row>
                 </Form.Row>
                 <Form.Row className="justify-content-center">
                     <Button type="submit" variant="warning" className="text-light">Enviar</Button>
